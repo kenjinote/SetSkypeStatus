3d71f9910fb9d2e50e79428d144afb35d8997a43
hack-jp <kenji256@gmail.com>
2015-05-21 20:12:50 +0900

    Add Files

---
+++ main.cpp
@@ -0,0 +1,147 @@
+#pragma comment(lib,"wtsapi32")
+
+#define _WIN32_WINNT 0x0501
+#pragma comment(linker,"/opt:nowin98")
+#include<windows.h>
+#include<wtsapi32.h>
+
+TCHAR szClassName[] = TEXT("SetSkypeStatus");
+
+DWORD SendSkypeMessage(HWND hWnd, HWND hGlobal_SkypeAPIWindowHandle, LPCSTR szMessage)
+{
+	COPYDATASTRUCT oCopyData;
+	oCopyData.dwData = 0;
+	oCopyData.lpData = (void *)szMessage;
+	oCopyData.cbData = lstrlenA(szMessage) + 1;
+	return SendMessage(hGlobal_SkypeAPIWindowHandle, WM_COPYDATA, (WPARAM)hWnd, (LPARAM)&oCopyData);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hGlobal_SkypeAPIWindowHandle = 0;
+	static UINT uiGlobal_MsgID_SkypeControlAPIAttach = 0;
+	static UINT uiGlobal_MsgID_SkypeControlAPIDiscover = 0;
+	switch (msg)
+	{
+	case WM_COPYDATA:
+		if (hGlobal_SkypeAPIWindowHandle == (HWND)wParam)
+		{
+			return 1;
+		}
+		break;
+	case WM_CREATE:
+		uiGlobal_MsgID_SkypeControlAPIAttach = RegisterWindowMessageA("SkypeControlAPIAttach");
+		uiGlobal_MsgID_SkypeControlAPIDiscover = RegisterWindowMessageA("SkypeControlAPIDiscover");
+		if (uiGlobal_MsgID_SkypeControlAPIAttach == 0 || uiGlobal_MsgID_SkypeControlAPIDiscover == 0)return -1;
+
+		while (!hGlobal_SkypeAPIWindowHandle)
+		{
+			SendMessage(HWND_BROADCAST, uiGlobal_MsgID_SkypeControlAPIDiscover, (WPARAM)hWnd, 0);
+			Sleep(10 * 1000);
+
+			MSG msg;
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+
+		PostMessage(hWnd, WM_WTSSESSION_CHANGE, WTS_REMOTE_CONNECT, 0);
+
+		WTSRegisterSessionNotification(hWnd, NOTIFY_FOR_ALL_SESSIONS);
+		break;
+	case WM_WTSSESSION_CHANGE:
+	{
+		if (hGlobal_SkypeAPIWindowHandle)
+		{
+			BOOL b = TRUE;
+			switch (wParam)
+			{
+			case WTS_REMOTE_CONNECT:
+			{
+				CHAR szComputerName[MAX_PATH];
+				lstrcpyA(szComputerName, "SET PROFILE MOOD_TEXT");
+				PWTS_SESSION_INFO ppSessionInfo = 0;
+				DWORD pCount;
+				if (WTSEnumerateSessions(WTS_CURRENT_SERVER_HANDLE, 0, 1, &ppSessionInfo, &pCount))
+				{
+					for (unsigned int i = 0; i<pCount; i++)
+					{
+						if (lstrcmp(ppSessionInfo[i].pWinStationName, TEXT("RDP-Tcp#0")) == 0)
+						{
+							DWORD bytesReturned = 0;
+							LPSTR pData = 0;
+							if (WTSQuerySessionInformationA(WTS_CURRENT_SERVER_HANDLE, ppSessionInfo[i].SessionId, WTSClientName, &pData, &bytesReturned))
+							{
+								lstrcatA(szComputerName, " ");
+								lstrcatA(szComputerName, pData);
+							}
+							WTSFreeMemory(pData);
+						}
+					}
+				}
+				(b = SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, "SET USERSTATUS ONLINE")) &&
+					(b = SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, szComputerName));
+			}
+				break;
+			case WTS_REMOTE_DISCONNECT:
+				(b = SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, "SET USERSTATUS AWAY")) &&
+					(b = SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, "SET PROFILE MOOD_TEXT"));
+				break;
+			}
+			if (!b)//‘—M‚ªƒ~ƒX‚Á‚½ê‡
+			{
+				SendMessage(HWND_BROADCAST, uiGlobal_MsgID_SkypeControlAPIDiscover, (WPARAM)hWnd, 0);
+			}
+		}
+	}
+		break;
+	case WM_DESTROY:
+		if (hGlobal_SkypeAPIWindowHandle)
+		{
+			SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, "SET USERSTATUS AWAY");
+			SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, "SET PROFILE MOOD_TEXT");
+		}
+		WTSUnRegisterSessionNotification(hWnd);
+		PostQuitMessage(0);
+		break;
+	default:
+		if (uiGlobal_MsgID_SkypeControlAPIAttach&&msg == uiGlobal_MsgID_SkypeControlAPIAttach)
+		{
+			if (lParam == 0)
+			{
+				hGlobal_SkypeAPIWindowHandle = (HWND)wParam;
+			}
+			return 1;
+		}
+		else
+		{
+			return DefWindowProc(hWnd, msg, wParam, lParam);
+		}
+	}
+	return 0;
+}
+EXTERN_C void __cdecl WinMainCRTStartup()
+{
+	MSG msg = { 0 };
+	HANDLE hMutex = CreateMutex(NULL, FALSE, szClassName);
+	if (GetLastError() != ERROR_ALREADY_EXISTS)
+	{
+		HINSTANCE hInstance = GetModuleHandle(0);
+		WNDCLASS wndclass = { 0, WndProc, 0, 0, hInstance, 0, 0, 0, 0, szClassName };
+		RegisterClass(&wndclass);
+		CreateWindowEx(WS_EX_TOOLWINDOW, szClassName, 0, 0, 0, 0, 0, 0, 0, 0, hInstance, 0);
+		while (GetMessage(&msg, 0, 0, 0))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		ReleaseMutex(hMutex);
+	}
+	CloseHandle(hMutex);
+	ExitProcess(msg.wParam);
+}
+#if _DEBUG
+void main(){}
+#endif
