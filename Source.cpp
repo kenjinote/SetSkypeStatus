55fe86da9cefbce5ef0385964262fa61d79c7c34
kenji <kenji256@gmail.com>
2018-08-10 16:56:37 +0900

    Skypeのバージョン8でクラシックAPIが使用できなくなったので、苦渋の処理

--- Source.cpp
+++ Source.cpp
@@ -1,144 +1,310 @@
 ﻿#pragma comment(lib,"wtsapi32")
+#pragma comment(lib,"psapi")
 
 #include <windows.h>
+#include <psapi.h>
 #include <wtsapi32.h>
 
 TCHAR szClassName[] = TEXT("SetSkypeStatus");
 
-DWORD_PTR SendSkypeMessage(HWND hWnd, HWND hGlobal_SkypeAPIWindowHandle, LPCWSTR szMessage)
+BOOL GetExeFilePathFromWindowHandle(HWND hWnd, LPTSTR lpszExePath, int szPathSize)
 {
-	DWORD dwStringLengthA = WideCharToMultiByte(CP_UTF8, 0, szMessage, -1, 0, 0, 0, 0);
-	LPSTR lpszStringA = (char*)GlobalAlloc(GPTR, dwStringLengthA);
-	WideCharToMultiByte(CP_UTF8, 0, szMessage, -1, lpszStringA, dwStringLengthA, 0, 0);
-	COPYDATASTRUCT oCopyData;
-	oCopyData.dwData = 0;
-	oCopyData.lpData = (void*)lpszStringA;
-	oCopyData.cbData = dwStringLengthA;
-	const DWORD_PTR dwReturn = SendMessage(hGlobal_SkypeAPIWindowHandle, WM_COPYDATA, (WPARAM)hWnd, (LPARAM)&oCopyData);
-	GlobalFree(lpszStringA);
-	return dwReturn;
+	DWORD dwProcessID = 0;
+	GetWindowThreadProcessId(hWnd, &dwProcessID);
+	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, dwProcessID);
+	if (!hProcess)
+		return FALSE;
+	HMODULE hModule = NULL;
+	DWORD dwDummy = 0;
+	if (!EnumProcessModules(hProcess, &hModule, sizeof(HMODULE), &dwDummy))
+		return FALSE;
+	if (!GetModuleFileNameEx(hProcess, hModule, lpszExePath, szPathSize))
+		return FALSE;
+	CloseHandle(hProcess);
+	return TRUE;
+}
+
+BOOL IsSkypeWindow(HWND hWnd)
+{
+	TCHAR szModulePath[MAX_PATH] = { 0 };
+	GetExeFilePathFromWindowHandle(hWnd, szModulePath, _countof(szModulePath));
+	if (wcsstr(szModulePath, L"Skype.exe"))
+	{
+		return TRUE;
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK EnumWindowsProcForSkype(HWND hWnd, LPARAM lParam)
+{
+	TCHAR szWindowTitle[256];
+	TCHAR szClassName[256];
+	GetWindowText(hWnd, szWindowTitle, _countof(szWindowTitle));
+	szWindowTitle[5] = 0;
+	GetClassName(hWnd, szClassName, _countof(szClassName));
+	if (lstrcmp(szWindowTitle, TEXT("Skype")) == 0 && lstrcmp(szClassName, TEXT("Chrome_WidgetWin_1")) == 0 && IsSkypeWindow(hWnd))
+	{
+		*((HWND*)lParam) = hWnd;
+		return FALSE; // 列挙中止
+	}
+	return TRUE; // 列挙続行
+}
+
+BOOL CALLBACK EnumWindowsProcForSkypeTray(HWND hWnd, LPARAM lParam)
+{
+	TCHAR szClassName[256];
+	GetClassName(hWnd, szClassName, _countof(szClassName));
+	if (lstrcmp(szClassName, TEXT("Electron_NotifyIconHostWindow")) == 0 && IsSkypeWindow(hWnd))
+	{
+		*((HWND*)lParam) = hWnd;
+		return FALSE; // 列挙中止
+	}
+	return TRUE; // 列挙続行
+}
+
+HWND GetSkypeWindow()
+{
+	HWND hSkypeWnd = NULL;
+	EnumWindows(EnumWindowsProcForSkype, (LPARAM)&hSkypeWnd);
+	return hSkypeWnd;
+}
+
+HWND GetSkypeTrayWindow()
+{
+	HWND hSkypeTrayWnd = NULL;
+	EnumWindows(EnumWindowsProcForSkypeTray, (LPARAM)&hSkypeTrayWnd);
+	return hSkypeTrayWnd;
+}
+
+VOID Login(LPCWSTR lpszStatusName)
+{
+	HWND hSkypeWnd = GetSkypeWindow();
+	if (hSkypeWnd)
+	{
+		SetForegroundWindow(hSkypeWnd);
+		HWND hRenderWidgetWnd = FindWindowEx(hSkypeWnd, NULL, TEXT("Chrome_RenderWidgetHostHWND"), NULL);
+		if (hRenderWidgetWnd == NULL)
+		{
+			HWND hTrayWnd = GetSkypeTrayWindow();
+			if (hTrayWnd)
+			{
+				SendMessage(hTrayWnd, WM_USER + 31745, 0x3, 0x201);
+			}
+			Sleep(100);
+			hRenderWidgetWnd = FindWindowEx(hSkypeWnd, NULL, TEXT("Chrome_RenderWidgetHostHWND"), NULL);
+		}
+		if (hRenderWidgetWnd)
+		{
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_ESCAPE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_ESCAPE, 0);
+
+			Sleep(500);
+
+			SendMessage(hRenderWidgetWnd, WM_LBUTTONDOWN, 0, MAKELPARAM(1, 1));
+			SendMessage(hRenderWidgetWnd, WM_LBUTTONUP, 0, MAKELPARAM(1, 1));
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_SPACE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_SPACE, 0);
+
+			Sleep(500);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			if (lpszStatusName)
+			{
+				LPCWSTR p = lpszStatusName;
+				while (*p)
+				{
+					SendMessage(hRenderWidgetWnd, WM_CHAR, (WPARAM)*p, 0);
+					p++;
+				}
+			}
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_SPACE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_SPACE, 0);
+
+			Sleep(100);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_DOWN, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_DOWN, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_RETURN, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_RETURN, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_ESCAPE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_ESCAPE, 0);
+
+			Sleep(500);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_ESCAPE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_ESCAPE, 0);
+		}
+
+		SendMessage(hSkypeWnd, WM_CLOSE, 0, 0);
+	}
+}
+
+VOID Logout()
+{
+	HWND hSkypeWnd = GetSkypeWindow();
+	if (hSkypeWnd)
+	{
+		SetForegroundWindow(hSkypeWnd);
+		HWND hRenderWidgetWnd = FindWindowEx(hSkypeWnd, NULL, TEXT("Chrome_RenderWidgetHostHWND"), NULL);
+		if (hRenderWidgetWnd == NULL)
+		{
+			HWND hTrayWnd = GetSkypeTrayWindow();
+			if (hTrayWnd)
+			{
+				SendMessage(hTrayWnd, WM_USER + 31745, 0x3, 0x201);
+			}
+			Sleep(100);
+			hRenderWidgetWnd = FindWindowEx(hSkypeWnd, NULL, TEXT("Chrome_RenderWidgetHostHWND"), NULL);
+		}
+		if (hRenderWidgetWnd)
+		{
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_ESCAPE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_ESCAPE, 0);
+
+			Sleep(500);
+
+			SendMessage(hRenderWidgetWnd, WM_LBUTTONDOWN, 0, MAKELPARAM(1, 1));
+			SendMessage(hRenderWidgetWnd, WM_LBUTTONUP, 0, MAKELPARAM(1, 1));
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_SPACE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_SPACE, 0);
+
+			Sleep(500);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_DELETE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_DELETE, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_TAB, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_TAB, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_SPACE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_SPACE, 0);
+
+			Sleep(100);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_DOWN, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_DOWN, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_DOWN, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_DOWN, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_DOWN, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_DOWN, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_RETURN, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_RETURN, 0);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_ESCAPE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_ESCAPE, 0);
+
+			Sleep(500);
+
+			SendMessage(hRenderWidgetWnd, WM_KEYDOWN, VK_ESCAPE, 0);
+			SendMessage(hRenderWidgetWnd, WM_KEYUP, VK_ESCAPE, 0);
+		}
+		SendMessage(hSkypeWnd, WM_CLOSE, 0, 0);
+	}
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	static HWND hGlobal_SkypeAPIWindowHandle = 0;
-	static UINT uiGlobal_MsgID_SkypeControlAPIAttach = 0;
-	static UINT uiGlobal_MsgID_SkypeControlAPIDiscover = 0;
+	static BOOL bLogined;
+	static LONGLONG dwLastSessionChange;
 	switch (msg)
 	{
-	case WM_COPYDATA:
-		if (hGlobal_SkypeAPIWindowHandle == (HWND)wParam)
-		{
-#ifdef _DEBUG
-			COPYDATASTRUCT*pCopyData = (COPYDATASTRUCT*)lParam;
-			LPSTR lpszText = (LPSTR)GlobalAlloc(0, pCopyData->cbData);
-			lstrcpyA(lpszText, (LPSTR)(pCopyData->lpData));
-			OutputDebugStringA(lpszText);
-			OutputDebugString(TEXT("\r\n"));
-#endif
-			return 1;
-		}
-		break;
 	case WM_CREATE:
-		uiGlobal_MsgID_SkypeControlAPIAttach = RegisterWindowMessageA("SkypeControlAPIAttach");
-		uiGlobal_MsgID_SkypeControlAPIDiscover = RegisterWindowMessageA("SkypeControlAPIDiscover");
-		if (uiGlobal_MsgID_SkypeControlAPIAttach == 0 || uiGlobal_MsgID_SkypeControlAPIDiscover == 0)return -1;
-		while (!hGlobal_SkypeAPIWindowHandle)
-		{
-			SendMessage(HWND_BROADCAST, uiGlobal_MsgID_SkypeControlAPIDiscover, (WPARAM)hWnd, 0);
-			Sleep(10 * 1000);
-
-			MSG msg;
-			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-			{
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}
-		}
 		PostMessage(hWnd, WM_WTSSESSION_CHANGE, WTS_REMOTE_CONNECT, 0);
 		WTSRegisterSessionNotification(hWnd, NOTIFY_FOR_ALL_SESSIONS);
 		break;
 	case WM_WTSSESSION_CHANGE:
 		{
-			if (hGlobal_SkypeAPIWindowHandle)
-			{
-				BOOL bFailed = FALSE;
+				if (GetTickCount64() - dwLastSessionChange <= 1000) // 前回の SSESSION CHANGE から 1 秒以内なら無視
+				{
+					return 0;
+				}
 				switch (wParam)
 				{
 				case WTS_REMOTE_CONNECT:
-				{
-					WCHAR szComputerName[MAX_PATH];
-					lstrcpyW(szComputerName, L"SET PROFILE MOOD_TEXT");
-					PWTS_SESSION_INFO ppSessionInfo = 0;
-					DWORD pCount;
-					if (WTSEnumerateSessions(WTS_CURRENT_SERVER_HANDLE, 0, 1, &ppSessionInfo, &pCount))
 					{
-						for (unsigned int i = 0; i<pCount; i++)
+						WCHAR szComputerName[MAX_PATH] = { 0 };
+						PWTS_SESSION_INFO ppSessionInfo = 0;
+						DWORD pCount;
+						if (WTSEnumerateSessions(WTS_CURRENT_SERVER_HANDLE, 0, 1, &ppSessionInfo, &pCount))
 						{
-							if (lstrcmp(ppSessionInfo[i].pWinStationName, TEXT("RDP-Tcp#0")) == 0)
+							for (unsigned int i = 0; i<pCount; i++)
 							{
-								DWORD bytesReturned = 0;
-								LPWSTR pData = 0;
-								if (WTSQuerySessionInformationW(WTS_CURRENT_SERVER_HANDLE, ppSessionInfo[i].SessionId, WTSClientName, &pData, &bytesReturned))
+								if (lstrcmp(ppSessionInfo[i].pWinStationName, TEXT("RDP-Tcp#0")) == 0)
 								{
-									lstrcatW(szComputerName, L" ");
-									lstrcatW(szComputerName, pData);
+									DWORD bytesReturned = 0;
+									LPWSTR pData = 0;
+									if (WTSQuerySessionInformationW(WTS_CURRENT_SERVER_HANDLE, ppSessionInfo[i].SessionId, WTSClientName, &pData, &bytesReturned))
+									{
+										lstrcpyW(szComputerName, pData);
+									}
+									WTSFreeMemory(pData);
 								}
-								WTSFreeMemory(pData);
 							}
+							WTSFreeMemory(ppSessionInfo);
 						}
-						WTSFreeMemory(ppSessionInfo);
+						if (bLogined == FALSE)
+						{
+							Login(szComputerName);
+							bLogined = TRUE;
+						}
 					}
-					if (!SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, L"SET USERSTATUS ONLINE"))
-					{
-						bFailed = TRUE;
-					}
-					else if (!SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, szComputerName))
-					{
-						bFailed = TRUE;
-					}
-				}
-				break;
+					break;
 				case WTS_REMOTE_DISCONNECT:
-					if (!SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, L"SET USERSTATUS AWAY"))
+					if (bLogined == TRUE)
 					{
-						bFailed = TRUE;
-					}
-					else if (!SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, L"SET PROFILE MOOD_TEXT"))
-					{
-						bFailed = TRUE;
+						Logout();
+						bLogined = FALSE;
 					}
 					break;
 				}
-				if (bFailed)//送信がミスった場合
-				{
-					SendMessage(HWND_BROADCAST, uiGlobal_MsgID_SkypeControlAPIDiscover, (WPARAM)hWnd, 0);
-				}
-			}
+				dwLastSessionChange = GetTickCount64();
 		}
 		break;
 	case WM_DESTROY:
-		if (hGlobal_SkypeAPIWindowHandle)
-		{
-			SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, L"SET USERSTATUS AWAY");
-			SendSkypeMessage(hWnd, hGlobal_SkypeAPIWindowHandle, L"SET PROFILE MOOD_TEXT");
-		}
 		WTSUnRegisterSessionNotification(hWnd);
+		if (bLogined == TRUE)
+		{
+			Logout();
+			bLogined = FALSE;
+		}
 		PostQuitMessage(0);
 		break;
 	default:
-		if (uiGlobal_MsgID_SkypeControlAPIAttach&&msg == uiGlobal_MsgID_SkypeControlAPIAttach)
-		{
-			if (lParam == 0)
-			{
-				hGlobal_SkypeAPIWindowHandle = (HWND)wParam;
-			}
-			return 1;
-		}
-		else
-		{
-			return DefWindowProc(hWnd, msg, wParam, lParam);
-		}
+		return DefWindowProc(hWnd, msg, wParam, lParam);
 	}
 	return 0;
 }
